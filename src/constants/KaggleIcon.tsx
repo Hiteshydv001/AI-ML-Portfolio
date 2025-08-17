a71a4f2ad0cacf963289743cd6d8a27e5a9f8d6a
HITESH KUMAR <114931638+Hiteshydv001@users.noreply.github.com>
2025-08-18 00:33:58 +0530

    make it responsive-11

--- src/constants/KaggleIcon.tsx
+++ src/constants/KaggleIcon.tsx
@@ -1,21 +1,33 @@
-import { SVGProps } from "react";
+import { Icon, TablerIconsProps } from '@tabler/icons-react';
 
-export function IconBrandKaggle(props: SVGProps<SVGSVGElement>) {
+function IconBrandKaggleComponent(props: TablerIconsProps) {
+  const { 
+    stroke = 'currentColor',
+    strokeWidth = 2,
+    width = 24,
+    height = 24,
+    ...restProps
+  } = props;
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
+      stroke={typeof stroke === 'string' ? stroke : 'currentColor'}
+      strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
-      {...props}
+      {...restProps}
     >
       <path d="M4 4h4v16H4z" />
       <path d="M12 4h4l4 8-4 8h-4l4-8z" />
     </svg>
   );
-} 
+}
+
+IconBrandKaggleComponent.displayName = 'IconBrandKaggle';
+
+export const IconBrandKaggle: Icon = IconBrandKaggleComponent as unknown as Icon;
